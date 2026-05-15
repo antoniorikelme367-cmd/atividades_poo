3bc7945859bf43109a3d7342c2f63e94f1cc91d9
antoniorikelme367-cmd <antoniorikelme367@gmail.com>
2026-05-15 07:33:22 -0300

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,9 @@
+
+class Carro {
+}
+string marca;
+void acelerar ()
+
+void ligar ()
+
+void frea ()
---
+++ test.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args){
+  }
+   Aluno a1 = new Aluno("Davi", 8.0, 7.5);
+    Aluno a2 = new Aluno("Rikelme", 9.5, 10.0);
+     Aluno a3 = new Aluno("Kaick", 9.5, 10.0);
+    
+    
+  
